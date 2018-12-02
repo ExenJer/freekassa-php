d086b1a200d228a45f36a7de8d7825b8be8123d8
ExenJer <exenjers@gmail.com>
2018-12-02 17:39:42 +0300

    Added dependency to FreeKassaHandler

--- src/FreeKassaHandler.php
+++ src/FreeKassaHandler.php
@@ -11,23 +11,31 @@ use ExenJer\FreeKassaPhp\Models\Payment;
 class FreeKassaHandler
 {
     /**
+     * @var array
+     */
+    private $request;
+
+    public function __construct(array $request)
+    {
+        $this->request  = $request;
+    }
+
+    /**
      * @param FreeKassaSetup $freeKassa
      * @return Payment
      * @throws NotValidSignException
      */
     public function handlePayment(FreeKassaSetup $freeKassa): Payment
     {
-        $request = $_REQUEST;
-
         $payment = new Payment();
-        $payment->setMerchantID(intval($request['MERCHANT_ID']));
-        $payment->setOrderID($request['MERCHANT_ORDER_ID']);
-        $payment->setAmount(floatval($request['AMOUNT']));
-        $payment->setOperationID(intval($request['intid']));
-        $payment->setPayerEmail($request['P_EMAIL']);
-        $payment->setPayerPhone($request['P_PHONE']);
-        $payment->setCurrencyID(intval($request['CUR_ID']));
-        $payment->setSign($request['SIGN']);
+        $payment->setMerchantID(intval($this->request['MERCHANT_ID']));
+        $payment->setOrderID($this->request['MERCHANT_ORDER_ID']);
+        $payment->setAmount(floatval($this->request['AMOUNT']));
+        $payment->setOperationID(intval($this->request['intid']));
+        $payment->setPayerEmail($this->request['P_EMAIL']);
+        $payment->setPayerPhone($this->request['P_PHONE']);
+        $payment->setCurrencyID(intval($this->request['CUR_ID']));
+        $payment->setSign($this->request['SIGN']);
         $payment->setUsParameters($this->getAllUsParams());
 
         if (! $freeKassa->getSignService()
